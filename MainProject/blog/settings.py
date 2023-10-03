6fa9042998524c91e32a8d21413150239734c4a5
AmandaMuehlbauer <amuehlbauer038@gmail.com>
2023-10-03 11:18:50 -0500

    updated settings.py

--- MainProject/blog/settings.py
+++ MainProject/blog/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jidder.onrender.com']
 
 
 # Application definition
