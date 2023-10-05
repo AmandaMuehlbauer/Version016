0d634ba9b75c54711edbc600c44c8c6ef00b7c28
AmandaMuehlbauer <amuehlbauer038@gmail.com>
2023-10-04 22:32:43 -0500

    updated wsgi

--- MainProject/blog/wsgi.py
+++ MainProject/blog/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the DJANGO_SETTINGS_MODULE environment variable based on the environment
-if os.environ.get("DJANGO_ENV") == "production":
+if os.environ.get("DEPLOY_ENV") == "production":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.production_settings")
 else:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
