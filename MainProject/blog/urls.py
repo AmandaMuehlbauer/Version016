1157baab706c239ac87220320cdb5e3161b72599
AmandaMuehlbauer <amuehlbauer038@gmail.com>
2023-10-16 18:28:17 -0500

    updated urls.py to serve media files in production

--- MainProject/blog/urls.py
+++ MainProject/blog/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
+
 
 
 
@@ -33,4 +36,7 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
