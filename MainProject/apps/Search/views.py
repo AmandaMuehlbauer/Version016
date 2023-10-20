be0f9b1b525e6cea69d6f094fc7779bf761fcde2
AmandaMuehlbauer <amuehlbauer038@gmail.com>
2023-10-19 22:53:55 -0500

    Updated code so search is simple query of title, removed elasticsearch function

--- MainProject/apps/Search/views.py
+++ MainProject/apps/Search/views.py
@@ -17,28 +17,48 @@ import socket
 
 #this is a basic search/lookup with the database. Not used in Jidder
 
-def elastic_search_view(request):
+def search_view(request):
     form = SearchForm()
     results = []
+    objects = []
 
 
     if request.method == 'GET':
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            print("query:")
+            print(query)
             results = Post.objects.filter(title__icontains=query)
+            print("results:")
+            print(results)
+
+                        # Extract primary keys (id) from the Post objects
+          # Look up full Post objects using the primary keys
+            pk_values = [post.id for post in results]
+        if pk_values:
+            objects = Post.objects.filter(id__in=pk_values)
+
+            # Generate URLs for each retrieved Post object
+            for post in objects:
+                post.url = reverse('core:post', args=[str(post.id), post.slug])
+                print(post.url)
         
         context = {
             'form': form,
-            'results': results
+            'results': results,
+            'objects': objects
         }
+        print("context:")
+        print(context)
 
-    return render(request, 'search/elastic_search_results.html', context)
+    return render(request, 'search/search_results.html', context)
+
 
 
 #This is the function that is used in the Jidder searchbar. It uses elasticsearch. 
 
-def elastic_search_view_draft001(request):
+def elastic_search_view(request):
     form = SearchForm(request.GET)
     results = []
     objects = []  # This stores the retrieved objects from the model
@@ -129,72 +149,6 @@ def check_port(host, port):
     sock.close()
 
 
-def elastic_search_view_draft002(request):
-
-    check_port("jidder-elasticsearch", 9200)
-    form = SearchForm(request.GET)
-    results = []
-    objects = []
-
-    if form.is_valid():
-        query = form.cleaned_data['query']
-
-        if os.environ.get("ENVIRONMENT") == "production":
-            es_server = 'jidder-elasticsearch:9200'
-        else:
-            es_server = 'localhost:9200'
-
-        try:
-            # Establish an Elasticsearch connection
-            client = Elasticsearch(hosts=[es_server])
-
-            # Ping the Elasticsearch server to check the connection
-            if not client.ping():
-                logging.error("Failed to connect to Elasticsearch server")
-                print("failed to connect to ES server")
-                return HttpResponse("Failed to connect to Elasticsearch server")
-
-            logging.info("Successfully connected to Elasticsearch server")
-            print("successfully connected to ES server")
-
-            # Build and execute the Elasticsearch search
-            response = client.search(
-                index='post',  # Replace with your index name
-                body={
-                    "query": {
-                        "multi_match": {
-                            "query": query,
-                            "fields": ["title", "content"]
-                        }
-                    }
-                }
-            )
-
-            results = response['hits']['hits']
-            print("results:")
-            print(results)
-
-            if request.user.is_authenticated:
-                # Save the search history to the database
-                search_history = SearchHistory(user=request.user, query=query)
-                search_history.save()
-
-            objects = Post.objects.filter(id__in=[result['_id'] for result in results])
-
-        except ConnectionError:
-            logging.error("Failed to connect to Elasticsearch server")
-            return HttpResponse("Failed to connect to Elasticsearch server")
-        except ElasticsearchException as e:
-            logging.error(f"An error occurred when executing the Elasticsearch query: {e}")
-            print(f"An error occurred when executing the Elasticsearch query: {e}")
-
-    context = {
-        'form': form,
-        'results': results,
-        'objects': objects,
-    }
-
-    return render(request, 'search/elastic_search_results.html', context)
 
 
 
