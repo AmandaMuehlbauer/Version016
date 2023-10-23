69623e250c420b3825c4bfac659fb85ba7c934e4
AmandaMuehlbauer <amuehlbauer038@gmail.com>
2023-10-23 17:33:20 -0500

    updated urls.py

--- MainProject/blog/urls.py
+++ MainProject/blog/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('', include(('apps.Search.urls', 'Search'), namespace='Search')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
