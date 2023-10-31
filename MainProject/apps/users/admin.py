fb9d8166ab54eade42bfe7943425b8cd19813b33
AmandaMuehlbauer <amuehlbauer038@gmail.com>
2023-10-31 17:05:51 -0500

    Updated custom user for admin panel

--- MainProject/apps/users/admin.py
+++ MainProject/apps/users/admin.py
@@ -3,7 +3,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+
+#class UserAdmin(BaseUserAdmin):
+ #   list_display = ('email', 'username', 'is_active', 'is_staff',)
+
 class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('username',)}),  # Exclude 'first_name' and 'last_name'
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important Dates', {'fields': ('last_login',)}),  # Exclude 'date_joined'
+    )
     list_display = ('email', 'username', 'is_active', 'is_staff')
+
 
 admin.site.register(User, UserAdmin)
