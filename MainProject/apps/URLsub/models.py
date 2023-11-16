a606bc2cfb8cefe6560fbb48f9e1496056cc5773
AmandaMuehlbauer <amuehlbauer038@gmail.com>
2023-11-16 16:26:29 -0600

    updated migrations

--- MainProject/apps/URLsub/models.py
+++ MainProject/apps/URLsub/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ..core.models import Tag  # Check if the import path is correct
+#from ..core.models import Tag  # Check if the import path is correct
 from taggit.managers import TaggableManager
 from django.conf import settings
 
