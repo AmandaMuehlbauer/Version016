876e256997c9b91ae11d3d8d5a900f27beeee045
AmandaMuehlbauer <amuehlbauer038@gmail.com>
2023-11-21 22:22:43 -0600

    corrected formatting for tags

--- MainProject/apps/ContactUs/admin.py
+++ MainProject/apps/ContactUs/admin.py
@@ -1,5 +1,13 @@
+#apps/ContactUs/admin.py
 from django.contrib import admin
 from .models import Contact
 
 # Register your models here.
-admin.site.register(Contact)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'content')
+    search_fields = ('name', 'email', 'content')
+    list_filter = ('name', 'email')
+
+admin.site.register(Contact, ContactAdmin)
--- MainProject/apps/ContactUs/models.py
+++ MainProject/apps/ContactUs/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib import admin
+
+
 
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=300)
     email = models.EmailField(max_length=100)
     content = models.TextField()
+
